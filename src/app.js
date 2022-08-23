57df8e71d557f801867839bc9e03be46218bf143
LALA3194 <109686903+LALA3194@users.noreply.github.com>
2022-08-23 16:40:04 -0400

    Added api/axios/ and added current temperature

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#temp-description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  console.log(response);
+}
+
+let apiKey = "25ff250e18c21d3ed78d0fa66517f0aa";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeatherCondition);
