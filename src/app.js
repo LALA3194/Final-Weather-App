76b12f06f50736ad712a2e5ab9059431041a346a
LALA3194 <109686903+LALA3194@users.noreply.github.com>
2022-09-07 08:14:38 -0400

    Fixed api key

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "25ff250e18c21d3ed78d0fa66517f0aa";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
